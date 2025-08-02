b57c9cdc2b0c652206ebfe3d31589d6b6cc384f4
Aftab Nadaf <nadafaftab97@gmail.com>
2025-08-02 22:03:20 +0530

    For the same problem, now I have added one more function which is optimal with TC as O(n) using two pointers approach.

--- ARRAYS/TWO POINTERS PATTERN/EASY/SquaresOfSortedArray.py
+++ ARRAYS/TWO POINTERS PATTERN/EASY/SquaresOfSortedArray.py
@@ -12,7 +12,24 @@ class Solution:
         
         return sorted(result)
     
+    def sortedSquaresOptimal(self, nums: List[int]) -> List[int]:
+        i = 0
+        j = len(nums) - 1
+        k = len(nums) - 1
+        result = [0] * len(nums)
+
+        while i <= j:
+            if abs(nums[i]) < abs(nums[j]):
+                result[k] = nums[j] ** 2
+                j -= 1
+            else:
+                result[k] = nums[i] ** 2
+                i += 1
+            k -= 1
+        
+        return result
+    
 nums = [-4,-1,0,3,10]
 
 s = Solution()
-print ( s.sortedSquares(nums))
+print ( s.sortedSquaresOptimal(nums))
